f4be244e7f0dda466307f847a655035781ab6cdf
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-07-02 19:09:40 +0600

    used installed router dom inside className "App" added : Route with paths: home, about, contacts, delivery and notfound

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<h1>About</h1>} />
+        <Route path="/contacts" element={<h1>Contacts</h1>} />
+        <Route path="/delivery" element={<h1>Delivery</h1>} />
+
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
