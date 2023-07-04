0f2cb1d8bf0e947af564eb929c7629fc4c9f773a
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-07-04 22:17:04 +0600

    inside file "App,.js" were added new two imports: Home and About

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+import {Routes, Route} from "";
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<h1>About</h1>} />
-        <Route path="/contacts" element={<h1>Contacts</h1>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contacts" element={<Contacts/>} />
         <Route path="/delivery" element={<h1>Delivery</h1>} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
+      
     </div>
   );
 }
