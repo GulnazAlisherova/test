94b1761cdd5ee16cd908dc8b8ca823b89361c065
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-07-04 22:18:40 +0600

    added some content: div with className, h1 and p tags with several contents for file "About.js" inside folder 'pages'

---
+++ src/components/pages/About.js
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div className="About">
+      <h1>This is my About page</h1>
+      <p>Here we tell you about our company</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto veniam laudantium quaerat corporis optio, dignissimos placeat magnam omnis commodi asperiores reiciendis nemo officiis. Debitis, repellat. Ex possimus sequi aperiam ad.</p>
+    </div>
+  );
+}
