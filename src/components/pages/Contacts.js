9fb5c1a46939e9600953cf097656382a82efe4d5
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-07-05 20:06:07 +0600

    filled file : Contacts.js with main tags: div with className and tags p with several contents

---
+++ src/components/pages/Contacts.js
@@ -0,0 +1,9 @@
+export default function Contacts() {
+  return (
+    <div className="Contacts">
+      <h1>This is my Contacts page</h1>
+      <p>Here we tell you Contacts our company</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto veniam laudantium quaerat corporis optio, dignissimos placeat magnam omnis commodi asperiores reiciendis nemo officiis. Debitis, repellat. Ex possimus sequi aperiam ad.</p>
+    </div>
+  );
+}
