755e08db3ca0827ae1e5faea28ad8665727f74b7
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-07-06 23:23:14 +0600

    for file "Delivery.js" inside folder "pages" were added new tags: div with className, and p tags with several contents

---
+++ src/components/pages/Delivery.js
@@ -0,0 +1,9 @@
+export default function Delivery() {
+  return (
+    <div className="Delivery">
+      <h1>This is my Delivery page</h1>
+      <p>Here we tell you Delivery our company</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto veniam laudantium quaerat corporis optio, dignissimos placeat magnam omnis commodi asperiores reiciendis nemo officiis. Debitis, repellat. Ex possimus sequi aperiam ad.</p>
+    </div>
+  );
+}
