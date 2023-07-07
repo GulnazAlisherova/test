54fe3ff6cb05d44327b241d8f76610273883740e
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-07-07 16:16:26 +0600

    added first main tags: h1, div with className and tags p with lorems  for file "Home.js" inside folder "pages"

---
+++ src/components/pages/Home.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="Home">
+      <h1>This is my Home page</h1>
+      <p>Here we tell you Home our company</p>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto veniam laudantium quaerat corporis optio, dignissimos placeat magnam omnis commodi asperiores reiciendis nemo officiis. Debitis, repellat. Ex possimus sequi aperiam ad.</p>
+    </div>
+  );
+}
