1ad43b95936e6a3ab1d61233c59547cff4e0b722
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-07-09 20:59:37 +0600

    for file "NotFound" inside folder pages added and filled first main tags: div and h1 + one comment

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="NotFound">
+      <h1>This is my NotFound page</h1>
+      {/* <image to={here must be a link to image}/> */}
+    </div>
+  );
+}
