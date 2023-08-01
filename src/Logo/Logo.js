e1674198de8ed95932a46a76671441d23e5e7b4f
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-08-01 13:34:54 +0600

    added temporary logo link  image for checking

--- src/Logo/Logo.js
+++ src/Logo/Logo.js
@@ -1,12 +1,12 @@
 import "./Logo.css";
-// import logo from "../../assets/react.svg";
+import logo from "../../assets/error.png";
 import { NavLink } from "react-router-dom";
 
 export default function Logo() {
   return (
     <div className="Logo">
       <NavLink to="/">
-        {/* <img src={logo} alt="logo" className="logo" /> */}
+        <img src={logo} alt="logo" className="logo" />
       </NavLink>
     </div>
   );
