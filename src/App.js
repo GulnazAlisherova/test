8813a402500256690b8fb74e48c65c21ba05079c
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-08-09 14:36:42 +0600

    for file App.js added AppContext and wrapped tag div with className "App"

--- src/App.js
+++ src/App.js
@@ -6,20 +6,36 @@ import Delivery from "./components/pages/Delivery";
 import NotFound from "./components/pages/NotFound";
 import Layout from "./components/Layout/Layout";
 
+export const AppContext = createContext({
+  categories: [],
+  products: [],
+  orders: [],
+
+  // контекст для корзины
+  cart: {}, // содержимое корзинки
+  setCart: () => { }, // изменить содержимое корзики
+
+  user: null,
+});
+
 function App() {
   return (
     <div className="App">
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/delivery" element={<Delivery />} />
+      <AppContext.Provider
+        value={{ categories, products, cart, setCart, user, orders }}
+      >
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contacts" element={<Contacts />} />
+            <Route path="/delivery" element={<Delivery />} />
 
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
 
-      </Layout>
+        </Layout>
+      </AppContext.Provider>
     </div>
   );
 }
