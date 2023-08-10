c4697b1aba8f7bda2b59bd397d6754682bbe593b
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-08-10 21:50:55 +0600

    inside folder "components" created new two files CSS&HTML and for css files added style and for js added function

---
+++ src/components/CategoryList/CategoryList.js
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import "./CategoryList.css";
+import { NavLink } from "react-router-dom";
+import { AppContext } from "../../App";
+import AddCategory from "../AddCategory/AddCategory";
+import DeleteCategory from "../DeleteCategory/DeleteCategory";
+
+export default function CategoryList() {
+  const { categories } = useContext(AppContext);
+  const output = categories.map((category) => (
+    <li key={category.id}>
+      <NavLink to={"/category/" + category.slug}>{category.name}</NavLink>
+      <DeleteCategory category={category} />
+      
+    </li>
+  ));
+
+  return (
+    <div className="CategoryList">
+      <ul>{output}</ul>
+      <AddCategory />
+    </div>
+  );
+}
