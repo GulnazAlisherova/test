88a8431eaec29e0ec060cd7eed7aebd763d6dc40
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-08-11 19:11:45 +0600

    removed all extra's: tags inside function "CategoryList"

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -2,15 +2,12 @@ import { useContext } from "react";
 import "./CategoryList.css";
 import { NavLink } from "react-router-dom";
 import { AppContext } from "../../App";
-import AddCategory from "../AddCategory/AddCategory";
-import DeleteCategory from "../DeleteCategory/DeleteCategory";
 
 export default function CategoryList() {
   const { categories } = useContext(AppContext);
   const output = categories.map((category) => (
     <li key={category.id}>
       <NavLink to={"/category/" + category.slug}>{category.name}</NavLink>
-      <DeleteCategory category={category} />
       
     </li>
   ));
@@ -18,7 +15,6 @@ export default function CategoryList() {
   return (
     <div className="CategoryList">
       <ul>{output}</ul>
-      <AddCategory />
     </div>
   );
 }
