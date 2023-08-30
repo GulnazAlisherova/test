e02b859e0091f5fd123ad6cc2403bfe945110dac
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-08-31 01:17:10 +0600

    for file Delivery.js added new tag "img" and commented it

--- src/components/pages/Delivery.js
+++ src/components/pages/Delivery.js
@@ -3,6 +3,7 @@ export default function Delivery() {
     <div className="Delivery">
       <h1>This is my Delivery page</h1>
       <p>Here we tell you Delivery our company</p>
+      {/* <img></img> */}
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto veniam laudantium quaerat corporis optio, dignissimos placeat magnam omnis commodi asperiores reiciendis nemo officiis. Debitis, repellat. Ex possimus sequi aperiam ad.</p>
     </div>
   );
