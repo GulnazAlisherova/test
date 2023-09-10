fddda09ec80d239c1b261850e3c8dccbf4467a4d
Alisherova Gulnaz <alisherovagulnaz268@gmail.com>
2023-09-10 08:24:38 +0600

    found one bug and fixed it  from slug -> path

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -7,7 +7,7 @@ export default function CategoryList() {
   const { categories } = useContext(AppContext);
   const output = categories.map((category) => (
     <li key={category.id}>
-      <NavLink to={"/category/" + category.slug}>{category.name}</NavLink>
+      <NavLink to={"/category/" + category.path}>{category.name}</NavLink>
       
     </li>
   ));
